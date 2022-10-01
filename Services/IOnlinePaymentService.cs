6fa1a3898dc207c9eef04a0e5335dca998418455
Matheus Oliveira <matheus.hortolandia@gmail.com>
2022-10-01 13:57:12 -0300

    Add project files.

---
+++ Services/IOnlinePaymentService.cs
@@ -0,0 +1,7 @@
+﻿namespace ExercicioInterface.Services {
+    interface IOnlinePaymentService {
+
+        double PaymentFee (double amount);
+        double Interest (double amount, int months);
+    }
+}
